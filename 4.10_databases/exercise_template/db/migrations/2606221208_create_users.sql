85e0c8437be18f06b50b0981d4f18b72dbfb4654
Edward Withers <dearshrewdwit@gmail.com>
2026-06-24 11:33:04 +0100

    Delete 4.10_databases/exercise_template directory

--- 4.10_databases/exercise_template/db/migrations/2606221208_create_users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(
-  id SERIAL PRIMARY KEY,
-  first_name varchar(60),
-  last_name varchar(60),
-  email varchar(80)
-)
--- 4.10_databases/exercise_template/db/seeds/002_create_roles.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO roles (role_name)
-VALUES
-    ('teacher'),
-    ('student'),
-    ('admin')
-ON CONFLICT (role_name) DO NOTHING;
